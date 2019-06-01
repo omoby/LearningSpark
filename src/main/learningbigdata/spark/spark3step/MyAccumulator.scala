e2a1e286ccd42ece6363c3b28c5d686271bfd214
omoby <3165845957@qq.com>
2019-06-01 14:32:33 +0800

    自定义字符串累加器

---
+++ src/main/learningbigdata/spark/spark3step/MyAccumulator.scala
@@ -0,0 +1,13 @@
+package main.learningbigdata.spark.spark3step
+
+/**
+  * FileName: MyAccumulator
+  * Author:   hadoop
+  * Email:    3165845957@qq.com
+  * Date:     19-6-1 下午2:08
+  * Description:
+  *
+  */
+class MyAccumulator {
+
+}
---
+++ src/main/learningbigdata/spark/spark3step/TestMyAccumulator.scala
@@ -0,0 +1,13 @@
+package main.learningbigdata.spark.spark3step
+
+/**
+  * FileName: TestMyAccumulator
+  * Author:   hadoop
+  * Email:    3165845957@qq.com
+  * Date:     19-6-1 下午2:19
+  * Description:
+  *
+  */
+class TestMyAccumulator {
+
+}
