e89fa39af7ec7e63f3b654650f515381216cca1e
omoby <3165845957@qq.com>
2019-06-01 16:54:49 +0800

    StructuredStreamingWordCount

---
+++ src/main/learningbigdata/spark/spark3step/StructuredStreamingWordCount.scala
@@ -0,0 +1,13 @@
+package main.learningbigdata.spark.spark3step
+
+/**
+  * FileName: StructuredStreamingWordCount
+  * Author:   hadoop
+  * Email:    3165845957@qq.com
+  * Date:     19-6-1 下午3:34
+  * Description:
+  *
+  */
+object StructuredStreamingWordCount {
+
+}
