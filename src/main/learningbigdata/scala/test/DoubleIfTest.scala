30dc896c154bc14b24c02308350466b23132a8f5
omoby <3165845957@qq.com>
2019-08-01 22:06:12 +0800

    双重if判断语句和嵌套判断语句

---
+++ src/main/learningbigdata/scala/test/DoubleIfTest.scala
@@ -0,0 +1,25 @@
+package main.learningbigdata.scala.test
+
+/**
+  * FileName: DoubleIfTest
+  * Author:   hadoop
+  * Email:    3165845957@qq.com
+  * Date:     19-8-1 下午9:55
+  * Description:
+  *
+  */
+object DoubleIfTest {
+  def main(args: Array[String]): Unit = {
+    var x = 30
+    if ( x == 10){
+      println("x 的值为 10")
+    }else if(x ==20){
+      println("x 的值为 20")
+    }else if(x == 30){
+      println("x 的值为 30")
+    }else{
+      println("无法判断 x 的值")
+    }
+  }
+
+}
---
+++ src/main/learningbigdata/scala/test/NestingIfElse.scala
@@ -0,0 +1,22 @@
+package main.learningbigdata.scala.test
+
+/**
+  * FileName: NestingIfElse
+  * Author:   hadoop
+  * Email:    3165845957@qq.com
+  * Date:     19-8-1 下午10:02
+  * Description:
+  *
+  */
+object NestingIfElse {
+  def main(args: Array[String]): Unit = {
+    var x  = 20
+    var y = 30
+    if (x == 20){
+      if (y == 30){
+        println("x = 20, y = 30")
+      }
+    }
+  }
+
+}
